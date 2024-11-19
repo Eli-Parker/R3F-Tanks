4141f7c792fb46343fe2b933b47812812758c78e
eli-parker <92594528+Eli-Parker@users.noreply.github.com>
2024-11-19 15:04:48 -0700

    Add Leva and OrbitControls for Debug, update World component with instanced walls

--- src/World.jsx
+++ src/World.jsx
@@ -1,99 +1,147 @@
 /* eslint-disable react/no-unknown-property */
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Component } from 'react';
+import { folder, Leva, useControls } from 'leva';
+import { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import * as THREE from 'three';
 
 /**
- * World class represents the game world and manages the entities within it.
+ * World function represents the game world and manages the entities within it.
  * It handles adding and removing entities, starting and stopping the game loop,
  * and updating and rendering the game state.
- *
- * @extends Component
  */
-class World extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            entities: [],
-            isRunning: false,
-        };
-    }
+function World ()
+{
+    const [debug] = useState(window.location.hash === "#debug");
+    
 
-    addEntity(entity) {
-        this.setState((prevState) => ({
-            entities: [...prevState.entities, entity],
-        }));
-    }
+    const { wallPosX, wallPosY, wallPosZ } = useControls(
+    "Main World Ctrls",
+    {
 
-    removeEntity(entity) {
-        this.setState((prevState) => ({
-            entities: prevState.entities.filter((e) => e !== entity),
-        }));
-    }
+        "Walls": folder(
+        {
+            wallPosX: { value: 0, step: 0.01 },
+            wallPosY: { value: 1.45, step: 0.01 },
+            wallPosZ: { value: -0.22, step: 0.001 },
+        },
+        { collapsed: true }
+        ),
+    },
+    { collapsed: true }
+  );
 
-    start() {
-        this.setState({ isRunning: true });
-        this.gameLoop();
-    }
+    return ( <>
+        {/* Hide debug controls if we're not in debug mode */}
+        {!debug && <Leva hidden />}
+        <Canvas
+            className="r3f"
+            shadows
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                dpr: 2, // TODO dynamic DPR
+                position: [0, 30, 20],
+            }}
+        >
+            {/* Orbit controls, only if we're in debug mode */}
+            {debug && <OrbitControlsSnippet />}
 
-    stop() {
-        this.setState({ isRunning: false });
-    }
+            {/* Lighting */}
+            <ambientLight intensity={0.8} />
 
-    gameLoop() {
-        if (!this.state.isRunning) return;
+            <directionalLight
+                castShadow
+                intensity={2}
+                position={[0, 5, -1]}
+            />
+            {/* Floor plane */}
+            <mesh receiveShadow rotation={ [-Math.PI/2, 0, 0] }>
+                <planeGeometry args={[100, 100]} />
+                <meshStandardMaterial color='grey'/>
+            </mesh>
 
-        // Update game state
-        this.update();
+            {/* Cube */}
+            <Walls wide={28} tall={20} position={ [wallPosX, wallPosY, wallPosZ] }/>
 
-        // Render game state
-        this.renderWorld();
+            {/* Cube */}
+            <mesh position={[0, 1, 0]} castShadow>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color='blue' />
+            </mesh>
 
-        // Continue the loop
-        requestAnimationFrame(this.gameLoop.bind(this));
-    }
-
-    update() {
-        // Update all entities
-        this.state.entities.forEach((entity) => {
-            if (entity.update) {
-                entity.update();
-            }
-        });
-    }
-
-    renderWorld() {
-        // Render all entities
-        this.state.entities.forEach((entity) => {
-            if (entity.render) {
-                entity.render();
-            }
-        });
-    }
-
-    render() {
-        return (
-            <Canvas
-                className="r3f"
-                camera={{
-                    fov: 45,
-                    near: 0.1,
-                    far: 20,
-                    dpr: 2, // TODO dynamic DPR
-                    position: [-3, 1.5, 6],
-                }}
-            >
-                <OrbitControlsSnippet />
-                <mesh>
-                  <boxGeometry args={[1, 1, 1]} />
-                  <meshBasicMaterial color='blue' />
-                </mesh>
-            </Canvas>
-        );
-    }
+        </Canvas>
+    </>);
 }
 
 export default World;
+
+/**
+ * Walls component creates the walls of the game world using instanced meshes.
+ * It positions the walls around 0,0 based on the provided width and height,
+ * and other basic props can be altered using the regular r3f params (eg position)
+ * 
+ * @component
+ * @param {Object} props - The properties object.
+ * @param {number} props.wide - The width of the walls.
+ * @param {number} props.tall - The height of the walls.
+ * @returns {JSX.Element} The rendered component.
+ */
+const Walls = ({wide, tall, ...props }) => {
+    const temp = new THREE.Object3D()
+    const instancedMeshRef = useRef()
+
+    // Create the instanced mesh
+    useEffect(() => 
+    {
+        // Set positions
+        let index = 0;
+
+        // Top and bottom walls
+        for (let i = -1 * (wide / 2); i < (wide / 2); i++) {
+            temp.position.set(i, 0, -tall / 2);
+            temp.updateMatrix();
+            instancedMeshRef.current.setMatrixAt(index++, temp.matrix);
+        }
+        for (let i = -1 * (wide / 2); i < (wide / 2); i++) {
+            temp.position.set(i, 0, tall / 2);
+            temp.updateMatrix();
+            instancedMeshRef.current.setMatrixAt(index++, temp.matrix);
+        }
+
+        // Left and right walls
+        for (let i = -1 * (tall / 2); i < (tall / 2); i++) {
+            temp.position.set(-wide / 2, 0, i);
+            temp.updateMatrix();
+            instancedMeshRef.current.setMatrixAt(index++, temp.matrix);
+        }
+        for (let i = -1 * (tall / 2); i < (tall / 2); i++) {
+            temp.position.set(wide / 2, 0, i);
+            temp.updateMatrix();
+            instancedMeshRef.current.setMatrixAt(index++, temp.matrix);
+        }
+
+      // Update the instance
+      instancedMeshRef.current.instanceMatrix.needsUpdate = true
+    },)
+
+    return (
+    <group {...props} >
+        <instancedMesh ref={instancedMeshRef} args={[null, null, (2 * wide + 2 * tall)]}>
+            <boxGeometry />
+            <meshPhongMaterial />
+        </instancedMesh>
+    </group>
+    )
+};
+
+Walls.propTypes = {
+    wide: PropTypes.number.isRequired,
+    tall: PropTypes.number.isRequired,
+};
+
 
 /**
  * Orbit controls snippet to hide away unnececary code.
@@ -103,12 +151,12 @@ const OrbitControlsSnippet = () => {
     return (
       <OrbitControls
         enableZoom={true}
-        enablePan={true}
+        // enablePan={true}
         enableRotate={true}
-        minAzimuthAngle={-Math.PI / 4}
-        maxAzimuthAngle={Math.PI / 4}
-        minPolarAngle={Math.PI / 6}
-        maxPolarAngle={Math.PI - Math.PI / 6}
+        // minAzimuthAngle={-Math.PI / 4}
+        // maxAzimuthAngle={Math.PI / 4}
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 2}
       />
     );
   };
